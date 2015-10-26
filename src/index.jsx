c6a88b1977448063d6cafe9a8e0d7cf6d9affee4
Warren Wong <wrrnwng@gmail.com>
2015-10-25 18:24:10 -0700

    complete simple ui

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Route} from 'react-router';
+import Router, {Route} from 'react-router';
 import App from './components/App';
 import Voting from './components/Voting';
+import Results from './components/Results';
 
 const pair = ['Trainspotting', '28 Days Later'];
 
 const routes = <Route component={App}>
+  <Route path="/results" component={Results} />
   <Route path="/" component={Voting} />
+}
 </Route>;
 
 ReactDOM.render(
-  <Voting pair={pair} />,
+  <Router>{routes}</Router>,
   document.getElementById('app')
 );
