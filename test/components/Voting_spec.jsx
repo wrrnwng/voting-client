38292d84169043716c4d7a89c50faf7ea26d5fbf
Warren Wong <wrrnwng@gmail.com>
2015-10-25 22:46:22 -0700

    add client side middleware

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 
 import {List} from 'immutable';
 
-import Voting from '../../src/components/Voting';
+import {Voting} from '../../src/components/Voting';
 import {expect} from 'chai';
 
 const {
